2e0702f76f41d4c66bc14c3e11658fb627e79d35
edgar256 <tinka.edgar@gmail.com>
2021-12-31 07:55:13 +0300

    Updates

---
+++ precache-manifest.91c8754078d62b3b0ca08333b352abba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfbba7847cef2183abf7a3cee7ba6464",
+    "url": "/god-frontend-code-test/index.html"
+  },
+  {
+    "revision": "73f070dd7924ff8f9c0e",
+    "url": "/god-frontend-code-test/static/css/main.a9eee94c.chunk.css"
+  },
+  {
+    "revision": "c76045d04693da62d5dd",
+    "url": "/god-frontend-code-test/static/js/2.ede4e753.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/god-frontend-code-test/static/js/2.ede4e753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73f070dd7924ff8f9c0e",
+    "url": "/god-frontend-code-test/static/js/main.23cbcc6b.chunk.js"
+  },
+  {
+    "revision": "e98a80c7338d0de738be",
+    "url": "/god-frontend-code-test/static/js/runtime-main.5994a7c7.js"
+  },
+  {
+    "revision": "f0a9bbd8ddc4d5bad25de0b50bee982b",
+    "url": "/god-frontend-code-test/static/media/chevron-circled.f0a9bbd8.svg"
+  },
+  {
+    "revision": "976cb13ef11cceba4e362176f31c3589",
+    "url": "/god-frontend-code-test/static/media/chevron-small.976cb13e.svg"
+  }
+]);
--- src/pages/Error.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Error() {
-    return (
-			<div className="container text-center">
-				<h1>Page Not Found</h1>
-			</div>
-		);
-}
